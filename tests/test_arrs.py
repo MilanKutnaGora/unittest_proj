fd13bea1256df6ba9a7a2ce721ced6fbc6689781
MilanKutnogorskiy <kutnogorskiy77@gmail.com>
2023-09-01 19:48:58 +0300

    Init

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -10,6 +10,7 @@ class TestArrs(unittest.TestCase):
         self.assertEqual(arrs.get([1, 2, 3], 2, "test"), 3)
 
 
+
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
